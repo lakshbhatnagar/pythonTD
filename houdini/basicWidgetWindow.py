455bd782de315eb3c95804a65827a52118afc229
Laksh Bhatnagar <lakshbhatnagar@users.noreply.github.com>
2018-10-04 10:54:18 +0530

    Create basicWidgetWindow.py
    PySide2 Example Window

---
+++ houdini/basicWidgetWindow.py
@@ -0,0 +1,27 @@
+from hutil.Qt.QtWidgets import *
+
+class MainWindow(QtWidgets.QWidget):
+    def __init__(self):
+        QWidget.__init__(self)
+        self.title = "MyFirstWindow"
+        self.left = 10
+        self.top = 10
+        self.width = 320
+        self.height = 200   
+        self.initUI()
+        
+    def initUI(self):
+        self.setWindowTitle(self.title)
+        self.setGeometry(self.left, self.top, self.width, self.height)
+        button = QtWidgets.QPushButton("Create Geo Node", self)
+        button.move(100,70) 
+        button.clicked.connect(self.on_click)
+        self.show()
+        
+    def on_click(self):
+        print('PyQt5 button click!')
+        import hou
+        hou.node("/obj").createNode("geo")
+        
+
+window = MainWindow()
