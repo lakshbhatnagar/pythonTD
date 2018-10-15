9097df66e3be6818d9b2a17489a394439ca49d84
Laksh Bhatnagar <lakshbhatnagar@users.noreply.github.com>
2018-10-15 09:18:19 +0530

    Update basicWidgetWindow.py

--- houdini/basicWidgetWindow.py
+++ houdini/basicWidgetWindow.py
@@ -1,8 +1,8 @@
-from hutil.Qt.QtWidgets import *
+from hutil.Qt import QtWidgets
 
 class MainWindow(QtWidgets.QWidget):
     def __init__(self):
-        QWidget.__init__(self)
+        QtWidgets.QWidget.__init__(self)
         self.title = "MyFirstWindow"
         self.left = 10
         self.top = 10
@@ -13,13 +13,13 @@ class MainWindow(QtWidgets.QWidget):
     def initUI(self):
         self.setWindowTitle(self.title)
         self.setGeometry(self.left, self.top, self.width, self.height)
-        button = QtWidgets.QPushButton("Create Geo Node", self)
+        button = QtWidgets.QPushButton("Hello", self)
         button.move(100,70) 
         button.clicked.connect(self.on_click)
         self.show()
         
     def on_click(self):
-        print('PyQt5 button click!')
+        print('PyQt5 button click')
         import hou
         hou.node("/obj").createNode("geo")
         
